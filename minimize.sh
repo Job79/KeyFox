a890c66901b67f491597199f91e93ff9e3e8979f
root <root@alpine.my.domain>
2021-01-07 20:33:18 +0100

    Upload install script + removed minimized versions

--- minimize.sh
+++
@@ -1,4 +0,0 @@
-find . -name "*.css" -not -name "*.min.css" -print0 | while read -d $'\0' file
-do
-   curl -X POST -s --data-urlencode "input@$file" https://cssminifier.com/raw > $(echo $file | sed -e 's/\.[^.]*$//').min.css
-done
